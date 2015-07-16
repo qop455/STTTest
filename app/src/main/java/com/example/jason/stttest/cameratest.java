cdbdfba78f115790be95fa461c3bfaf002b2eae5
Jason_Chiu <qop455@gmail.com>
2015-07-16 18:13:11 +0800

    init stt

---
+++ app/src/main/java/com/example/jason/stttest/cameratest.java
@@ -0,0 +1,44 @@
+package com.example.jason.stttest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+
+/**
+ * Created by Jason on 2015/7/16.
+ */
+public class cameratest extends Activity
+{
+    //宣告
+    private DisplayMetrics mPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //讀取手機解析度
+        mPhone = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(mPhone);
+
+        opencamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    private void opencamera(){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    }
+}
