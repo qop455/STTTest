8291d048a49a18f48d647eba43eb764db78db248
Jason_Chiu <qop455@gmail.com>
2015-08-17 14:40:11 +0800

    有bug的音量控制
    如題

--- app/src/main/java/com/example/jason/stttest/buttontest.java
+++ app/src/main/java/com/example/jason/stttest/buttontest.java
@@ -1,6 +1,8 @@
 package com.example.jason.stttest;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.media.AudioManager;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -26,28 +28,50 @@ public class buttontest extends YouTubeBaseActivity implements YouTubePlayer.OnI
     private YouTubePlayer player;
     private ImageButton play;
     private ImageButton pause;
+    private ImageButton normal;
+    private ImageButton vibrete;
+    private ImageButton volume_up;
+    private ImageButton volume_down;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_stttest);
         youTubeView=(YouTubePlayerView)findViewById(R.id.youtube_view);
         play=(ImageButton)findViewById(R.id.play);
         pause=(ImageButton)findViewById(R.id.pause);
-        youTubeView.initialize(API_KEY,this);
+        normal=(ImageButton)findViewById(R.id.normal);
+        vibrete=(ImageButton)findViewById(R.id.vibrete);
+        volume_up=(ImageButton)findViewById(R.id.volume_up);
+        volume_down=(ImageButton)findViewById(R.id.volume_down);
+        youTubeView.initialize(API_KEY, this);
 
         play.setOnClickListener(this);
         pause.setOnClickListener(this);
+        normal.setOnClickListener(this);
+        vibrete.setOnClickListener(this);
+        volume_up.setOnClickListener(this);
+        volume_down.setOnClickListener(this);
+        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
     }
 
     @Override
     public void onClick(View v) {
         if (v == play) {
             player.play();
-        } else if (v == pause) {
+        }else if (v == pause) {
             player.pause();
+        }else if (v== normal) {
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        }else if (v== vibrete) {
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+        }else if (v== volume_up) {
+            audioManager.adjustVolume(AudioManager.ADJUST_RAISE, 0);
+        }else if (v== volume_down) {
+            audioManager.adjustVolume(AudioManager.ADJUST_LOWER, 0);
         }
     }
 
@@ -74,5 +98,11 @@ public class buttontest extends YouTubeBaseActivity implements YouTubePlayer.OnI
                     getString(R.string.error_player), youTubeInitializationResult.toString());
             Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.pause();
     }
 }
